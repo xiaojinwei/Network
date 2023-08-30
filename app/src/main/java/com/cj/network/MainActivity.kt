ed9eb213d8fe1900fe56acb4239a18c488eb7e53
cj <cj_286@163.com>
2023-08-30 19:14:34 +0800

    去除自动初始化

--- app/src/main/java/com/cj/network/MainActivity.kt
+++ app/src/main/java/com/cj/network/MainActivity.kt
@@ -2,10 +2,13 @@ package com.cj.network
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import com.cj.network.http.ApiService
+import com.cj.network.http.GlobalConfiguration
 import com.cj.runtime.Network
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
+import com.cj.runtime.integration.ConfigModule
+import com.google.gson.Gson
+import kotlinx.coroutines.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -13,10 +16,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val tv = findViewById<TextView>(R.id.tv)
 
-        GlobalScope.launch {
+        Network.init(this, mutableListOf<ConfigModule>().apply {
+            add(GlobalConfiguration())
+        })
+
+        GlobalScope.launch(Dispatchers.IO) {
             val result = Network.repositoryManager.obtainRetrofitService(ApiService::class.java).getTopList()
             println("result:${result}")
+            tv.post {
+                tv.text = Gson().toJson(result)
+            }
         }
 
     }
